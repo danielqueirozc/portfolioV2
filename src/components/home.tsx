e5faf97f4f39c1cad4514852ee0a9d818c6bd7c2
danielqueirozc <danielqueiroz.job@gmail.com>
2024-09-30 12:43:50 -0300

    continuing the man

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,22 @@
 export function Home() {
     return (
-        <main className="px-4 py-8">
-            <p className="text-3xl font-semibold p-3">
+        <main className="px-4 py-8 flex flex-col gap-6">
+            <p className="text-3xl font-semibold p-3 text-white">
                 Daniel é <span className="text-purple">Web Designer</span> e <span className="text-purple">Desenvolvedor Front-end</span>
             </p>
+            <span className="text-gray">
+                Ele cria sites responsivos onde a tecnologia encontra a criatividade
+            </span>
+           <div>
+                <img src="../src/assets/home.png" alt="" />
+                <div className="flex p-2 gap-[10px] items-center justify-center">
+                    <div className="w-4 h-4 bg-purple" />
+                    <div className="flex-1 flex flex-col">
+                        <span className="text-gray font-medium">Atualmente trabalhando no </span>
+                        <span className="text-white font-bold">Portfólio</span>
+                    </div>
+                </div>
+           </div>
         </main>
     );
 }
